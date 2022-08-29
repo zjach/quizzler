67bf97d1b5603e693c507195999148df3461a14a
zjach <jackfontane@gmail.com>
2022-08-29 17:42:29 -0400

    Init 1742h

---
+++ Quizzler-iOS13/Question.swift
@@ -0,0 +1,19 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by PawanKumar K on 8/26/22.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+    
+    init(q: String, a: String) {
+        text = q
+        answer = a
+    }
+}
